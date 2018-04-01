d9dcc30bd3f4fa84238df97fa7bfa919ebc3b9b6
Binwei Xu <bixubot@gmail.com>
2018-04-01 16:37:04 -0400

    updated perceptron: worse than elasticnet

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,5 @@
 import scipy.io as sio
+import numpy as np
 from sklearn import linear_model
 import math
 
@@ -6,18 +7,19 @@ TRAIN_DIR = "data/md_train.mat"
 TEST_DIR = "data/md_test.mat"
 TEST_SIZE = 10000
 
-def main (alp, ratio):
+def main (alp):
     train_x, train_y, test_x, test_y = (sio.loadmat(TRAIN_DIR)['trainx'], sio.loadmat(TRAIN_DIR)['trainy'], sio.loadmat(TEST_DIR)['testx'], sio.loadmat(TEST_DIR)['testy'])
-    
-    clf = linear_model.Lasso()
+    train_y = train_y.ravel()
+    test_y = test_y.ravel()
+    clf = linear_model.Perceptron(penalty='elasticnet',alpha=alp,n_jobs=4, )
     clf.fit(train_x,train_y)
     years = clf.predict(test_x)
     
-    diff = 0
+    diff = float(0.0)
     for (i, j) in zip(years, test_y):
         diff += abs(i-j)
     diff /= TEST_SIZE   
-    print ("MSE is: " + str(diff) +"with alpha: "+str(alp)+", l1_ratio: "+str(ratio))
+    print ("MSE is: " + str(diff) +" with alpha: "+str(alp))
     return diff
     
 def predict(clf):
@@ -32,16 +34,13 @@ def predict(clf):
     file.close()
 
 if __name__ == "__main__":
-    alphas = [0.01,0.1,1]
-    ratios = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-    best_alpha, best_ratio = None, None
+    alphas = [0.005,0.01,0.05,0.1,0.2]
+    best_alpha = None
     diff = math.inf
     for alp in alphas:
-        for r in ratios:
-            temp = main(alp, r)
-            if temp < diff:
-                diff = temp
-                best_alpha = alp
-                best_ratio = r
-    print("optimal alpha is: "+str(best_alpha)+ " and l1_ratio is: "+str(best_ratio))
+        temp = main(alp)
+        if temp < diff:
+            diff = temp
+            best_alpha = alp
+    print("optimal alpha is: "+str(best_alpha))
 
