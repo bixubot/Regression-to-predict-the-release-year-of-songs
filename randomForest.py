6cb3bd3cba5ae2ba64e1ab69ead4ab7b5b755a44
Binwei Xu <bixubot@gmail.com>
2018-04-02 19:39:08 -0400

    updated randomforest, has the best performance so far

--- randomForest.py
+++ randomForest.py
@@ -1,15 +1,17 @@
 import scipy.io as sio
-from sklearn import linear_model
+from sklearn import linear_model, ensemble
 import math
 
 TRAIN_DIR = "data/md_train.mat"
 TEST_DIR = "data/md_test.mat"
 TEST_SIZE = 10000
 
-def main (alp, ratio):
+def main (n_trees, n_fea):
     train_x, train_y, test_x, test_y = (sio.loadmat(TRAIN_DIR)['trainx'], sio.loadmat(TRAIN_DIR)['trainy'], sio.loadmat(TEST_DIR)['testx'], sio.loadmat(TEST_DIR)['testy'])
-    
-    clf = linear_model.ElasticNet(alpha=alp, l1_ratio=ratio, normalize=False,warm_start=True)
+    train_y = train_y.ravel()
+    test_y = test_y.ravel()
+     
+    clf = ensemble.RandomForestRegressor(n_estimators=n_trees, n_jobs=4, min_impurity_split=0.5, max_features=n_fea)
     clf.fit(train_x,train_y)
     years = clf.predict(test_x)
     
@@ -17,39 +19,36 @@ def main (alp, ratio):
     for (i, j) in zip(years, test_y):
         diff += abs(i-j)
     diff /= TEST_SIZE   
-    print ("MSE is: " + str(diff) +"with alpha: "+str(alp)+", l1_ratio: "+str(ratio))
+    print ("MSE is: " + str(diff) +"with n_trees: "+str(n_trees)+" and n_features is: "+str(n_fea))
     
-    predict(clf)
-    
-    return diff
+    return diff, clf
     
 def predict(clf):
     train_x, train_y, test_x = (sio.loadmat('../MSdata.mat')['trainx'], sio.loadmat('../MSdata.mat')['trainy'], sio.loadmat('../MSdata.mat')['testx'])
-    
+    train_y = train_y.ravel()
+
     clf.fit(train_x, train_y)
     years = clf.predict(test_x)
 
     print ("Writing.......")
-    file = open('result_elastic.csv','w')
+    file = open('result_randomForest.csv','w')
     file.write("dataid,prediction\n")
     for i in range(len(years)):
         file.write("{}, {} \n".format(i + 1, years[i]))
     file.close()
 
 if __name__ == "__main__":
-    # optimal comb is 0.01, 0.9
-    #alphas = [0.01,0.1,1]
-    #ratios = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-    alphas = [0.01]
-    ratios = [0.9]
-    best_alpha, best_ratio = None, None
+    forests = [20,50,100,200,250]
+    n_feas = [40,50,60]
+    best_n, best_fea, best_clf = None, None, None
     diff = math.inf
-    for alp in alphas:
-        for r in ratios:
-            temp = main(alp, r)
+    for n_trees in forests:
+        for n_fea in n_feas:
+            temp, clf = main(n_trees, n_fea)
             if temp < diff:
                 diff = temp
-                best_alpha = alp
-                best_ratio = r
-    print("optimal alpha is: "+str(best_alpha)+ " and l1_ratio is: "+str(best_ratio))
-
+                best_n = n_trees
+                best_fea = n_fea
+                best_clf = clf
+    print("optimal number of trees is: "+str(best_n)+" and n_features is: "+str(best_fea))
+    #predict(best_clf)
--- ridge.py
+++ ridge.py
@@ -1,13 +1,33 @@
 import scipy.io as sio
 from sklearn import linear_model
 
+TRAIN_DIR = "data/md_train.mat"
+TEST_DIR = "data/md_test.mat"
+TEST_SIZE = 10000
+
 def main ():
-    train_x, train_y, test_x = (sio.loadmat('data/MSdata.mat')['trainx'], sio.loadmat('data/MSdata.mat')['trainy'], sio.loadmat('data/MSdata.mat')['testx'])
+    train_x, train_y, test_x, test_y = (sio.loadmat(TRAIN_DIR)['trainx'], sio.loadmat(TRAIN_DIR)['trainy'], sio.loadmat(TEST_DIR)['testx'], sio.loadmat(TEST_DIR)['testy'])
     clf = linear_model.RidgeCV(alphas = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0], normalize=True, cv=10, store_cv_values=False)
     print ("Training......")
     clf.fit(train_x,train_y)
     print ("Predicting.......")
     years = clf.predict(test_x)
+
+    diff = 0.0
+    for i,j in zip(test_y, years):
+        diff += abs(i-j)
+    diff /= TEST_SIZE
+    print("MSE is: "+str(diff))
+
+    # predict(clf)
+    
+
+def predict (clf):
+    train_x, train_y, test_x = (sio.loadmat("../MSdata.mat")["trainx"], sio.loadmat("../MSdata.mat")['trainy'], sio.loadmat('../MSdata.mat')['testx'])
+
+    clf.fit(train_x, train_y)
+    years = clf.predict(test_x)
+
     print("Writing.......")
     file = open('result_ridge.csv','w')
     file.write("dataid,prediction\n")
